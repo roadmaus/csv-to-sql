fb537900e12a081f14808c875b5346ebfcd1b976
roadmaus <47949813+roadmaus@users.noreply.github.com>
2023-12-07 14:09:28 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,17 @@
-@echo off
 
 REM Activate the virtual environment
+echo Activating virtual environment...
 call venv\Scripts\activate
+if %errorlevel% neq 0 echo Failed to activate virtual environment! && exit /b %errorlevel%
 
 REM Run the Python script
+echo Running Python script...
 python gui.py
+if %errorlevel% neq 0 echo Python script encountered an error! && exit /b %errorlevel%
 
 REM Optional: deactivate the virtual environment when done
+echo Deactivating virtual environment...
 call deactivate
+if %errorlevel% neq 0 echo Failed to deactivate virtual environment!
+
+echo Script execution completed.
